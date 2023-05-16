f1d310b46f3149440a28a2492f290b8da231be70
prime623 <patrik.zetovic23@gmail.com>
2023-05-16 09:51:09 +0200

    add check functions for approvals

--- frontend/src/api/blockchain/common.ts
+++ frontend/src/api/blockchain/common.ts
@@ -1,7 +1,9 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers, Signer } from "ethers";
 
 import { CHAIN_TO_RPC, SupportedChain } from "@/constants";
 import { executeAsync } from "../wrappers";
+import { SWAPTOR_ADDRESS } from "@/environment";
+import { getERC20Contract, getERC721Contract } from "@/utils/blockchain";
 
 export const getChainTime = async (chain: SupportedChain) => {
   const rpc = CHAIN_TO_RPC[chain];
@@ -13,4 +15,48 @@ export const getChainTime = async (chain: SupportedChain) => {
 
     return timestamp;
   });
+};
+
+export const checkERC20Allowance = async (
+  tokenAddress: string,
+  tokenData: string,
+  connectedAddress: string,
+  signer: Signer
+): Promise<boolean> => {
+  try {
+    console.log(tokenAddress, connectedAddress, SWAPTOR_ADDRESS);
+    const allowance = (
+      await getERC20Contract(tokenAddress)
+        .connect(signer)
+        .allowance(connectedAddress, SWAPTOR_ADDRESS)
+    ).toString();
+
+    console.log(allowance, tokenData);
+
+    return BigNumber.from(allowance).gte(BigNumber.from(tokenData));
+  } catch (e: any) {
+    console.log(e.message);
+    return false;
+  }
+};
+
+export const checkERC721Approval = async (
+  tokenAddress: string,
+  tokenData: string,
+  connectedAddress: string,
+  signer: Signer
+): Promise<boolean> => {
+  try {
+    const token = getERC721Contract(tokenAddress).connect(signer);
+    const ownerOf = await token.ownerOf(tokenData);
+    const approved = await token.getApproved(tokenData);
+
+    return (
+      ownerOf.toLowerCase() === connectedAddress &&
+      approved.toLowerCase() === SWAPTOR_ADDRESS!.toLowerCase()
+    );
+  } catch (e: any) {
+    console.log(e.message);
+    return false;
+  }
 };
