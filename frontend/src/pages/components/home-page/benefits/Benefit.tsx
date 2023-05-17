e98e52563d2ca13daca3961eed165dae70f6e2ea
Tesa Jurisic <tesa.jurisic1@gmail.com>
2023-05-17 19:28:12 +0200

    add icon for each benefit

--- frontend/src/pages/components/home-page/benefits/Benefit.tsx
+++ frontend/src/pages/components/home-page/benefits/Benefit.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 type BenefitProps = {
+  icon: any; // next recommends to use any for svg imports
   header: string;
   paragraph: string;
 };
 
-const Benefit = ({ header, paragraph }: BenefitProps) => {
+const Benefit = ({ icon, header, paragraph }: BenefitProps) => {
   return (
-    <div className="rounded-container flex flex-col text-center justify-center h-[18rem] p-8">
+    <div className="rounded-container flex flex-col text-center justify-center p-8">
+      <Image src={icon} alt={`${header} icon`} className="mx-auto mb-4" />
       <h1 className="text-2xl font-bold mb-5">{header}</h1>
       <span className="font-normal">{paragraph}</span>
     </div>
